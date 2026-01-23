b33e278508468ada34b13f1938bb935bf48c1124
Zahed <zahedm45@gmail.com>
2026-01-23 12:36:20 +0100

    Clean up api.py

--- src/mlops_group35/api.py
+++ src/mlops_group35/api.py
@@ -41,39 +41,6 @@ def root():
     return response
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-# @app.post("/predict")
-# def predict(data: PredictionInput, n_clusters: int):
-#     required_feats = data.model_fields.keys()
-#
-#     with initialize(config_path="../../configs", version_base="1.3"):
-#         cfg = compose(config_name="cluster")
-#
-#     train_cfg = train.build_train_config(cfg)
-#
-#     csv_path = "data/processed/combined.csv"
-#     df = load_preprocessed_data(csv_path, required_feats)
-#
-#     # Convert input to DataFrame
-#     new_row = pd.DataFrame([data.model_dump()])
-#     new_row = new_row[required_feats]
-#
-#     # Append to dataset
-#     df_with_new = pd.concat([df, new_row], ignore_index=True)
-#
-#     if n_clusters == 1 or n_clusters == 0:
-#         n_clusters = train_cfg.n_clusters
-#     df_out, kmeans, X_scaled = train.train(df_with_new, n_clusters, train_cfg.seed)
-#
-#     # Get user's cluster
-#     user_cluster = df_out.iloc[-1]["cluster"]
-#     # TODO ATM it returns the cluster number, but it should return some interpretations
-#     return {"Group": int(user_cluster)}
-
 
 @app.get("/drift")
 def drift(n: int = 200, psi_threshold: float = 0.2):
